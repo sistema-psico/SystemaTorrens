638019805626d21240ed7fb5ec8dfb120efc9e55
sistema-psico <sistema-psico@proton.me>
2026-01-26 10:36:47 -0300

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -15,4 +15,3 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
-export const storage = getStorage(app); // <--- 2. Exportar
